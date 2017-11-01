8d9717d64f10ed21f5cd297080679d26e16af71a
LBel <isabelrm.biofisica@gmail.com>
2017-11-01 14:24:46 -0600

    changing test path

--- interpolatingDetection/test.py
+++ interpolatingDetection/test.py
@@ -1,6 +1,6 @@
 from interpDetect import interpDetect
 
-rawpath = '/media/albert/DATA/data/'
-fileName = 'P29_16_05_14_retina01_right_stim1_smallarray_whitenoise100msHDF5.brw'
+rawpath = '/home/isabel/BRW/120116/'
+fileName = '01control120116.brw'
 
 interpDetect(rawpath + fileName)
